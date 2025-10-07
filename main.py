bcdf046914b7470e7e2afafe762a9f2e4688cc8b
trollian-alien <epciscientician@gmail.com>
2025-10-07 15:53:41 +0300

    fixed some stuff and added a --verbose tag that makes this clanker talk too much

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import os, sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-# python
 print("API key present:", bool(api_key))
-
-from google import genai
 
 client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from ai-agent!")
+    if len(sys.argv) < 2:
+        print("Error, no prompt provided. Usage: python3 main.py your_prompt")
+        sys.exit(1)
+
+    user_prompt = " ".join(sys.argv[1:])
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    contents=messages
     )
-    print(response.text)
 
     usage = response.usage_metadata
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    if sys.argv[-1] == "--verbose":
+        print("User prompt: {user_prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
+    print(response.text)
     
     
 
