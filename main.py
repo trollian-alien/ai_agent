a3b23f0f0a45e5655218509cdde0817b8450f1b6
trollian-alien <epciscientician@gmail.com>
2025-10-07 12:42:43 +0300

    ok we can actually start doing stuff with this now lol

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os, sys
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+# python
+print("API key present:", bool(api_key))
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
 def main():
     print("Hello from ai-agent!")
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', 
+    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+    print(response.text)
+
+    usage = response.usage_metadata
+    print(f"Prompt tokens: {usage.prompt_token_count}")
+    print(f"Response tokens: {usage.candidates_token_count}")
+    
+    
+
 
 
 if __name__ == "__main__":
